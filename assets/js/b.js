ed2f44753d3993bc77d20ba23fd8b06c03262f13
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-05-08 11:25:25 +0100

    ----
    ----

--- assets/js/b.js
+++ assets/js/b.js
@@ -1,15 +1,4 @@
 
-
-function signup(){
-    alert ("Sorry! website still under construction, \r\n \r\n You will get updated when it is done,\r\n keep a tab on my social media handles, \r\n \r\n © OLARINDE BUKUNMI 2022");
-  }
-  
-  function dimmode(){
-     alert("Oops! darkmode unavailable at the moment");
-     }
-     
-
-
 let barButton=document.querySelector('.bars');
 let headerUl=document.querySelector('header ul');
 let closebar=document.querySelector(".closebar");
@@ -66,53 +55,5 @@ function rotate(){
 
 //setInterval(rotate,5000);
 }
-
-
-
-
-
-
- /*		
-//original
-var slideIndex = 1;
-showSlides(slideIndex); 
-
-function plusSlides_(n) {
-showSlides(slideIndex+= n);      
-}
-/*
-slideindex=1  (first)
-slideindex=slideindex+n;
-slideindex=1+1=2;   (second)
-slideindex=2+1=3;   (third)
-
-slideindex=3+1=4;    ->slideindex=1  (first)
-
-slideindex=-1+1=0;   ->slideindex=3   (third)
-
-function currentSlide(n) { 
-showSlides(slideIndex = n);
-}
-// slideindex=n
-
-function showSlides(n) {
-var i;
-var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
-if (n > slides.length) {slideIndex = 1}  //if n=4 ? slideindex=1 :  
-if (n < 1) {slideIndex = slides.length} ; //if n=0 ? slideindex=3
-
-for (i = 0; i < slides.length; i++) {
-slides[i].style.display = "none";  //(0,1,2)all will be none here
-}
-
-for (i = 0; i < dots.length; i++) {
-dots[i].classList.remove("active");
-}
-
-slides[slideIndex-1].style.display = "block"; //1-1 =0, 2-1=1, 3-1=2
-dots[slideIndex-1].classList.add("active");
-}
-*/
 
 
